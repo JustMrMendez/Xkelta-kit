6e11abdde4a63357a9b1f34451a2f527825a891f
JustMrMendez <mrmendez@amvestor.com>
2023-03-23 14:18:06 -0400

    switch defaultOptions to db.ts instead of exporting generated xata.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,16 @@
-import { defaultOptions, XataClient } from './xata';
+import { XataClient } from './xata';
 import { BaseClient } from '@xata.io/client';
+import { XATA_API_KEY, XATA_BRANCH, XATA_URL } from '$env/static/private';
+
+export const defaultOptions = {
+	databaseUrl: XATA_URL,
+	apiKey: XATA_API_KEY,
+	branch: XATA_BRANCH
+};
 
 export const client = new XataClient({
-	branch: defaultOptions.branch,
-	apiKey: defaultOptions.apiKey
+	apiKey: XATA_API_KEY,
+	branch: XATA_BRANCH
 });
 
 export const baseClient = new BaseClient({
